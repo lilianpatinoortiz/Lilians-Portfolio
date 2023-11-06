01eda191cbf70cf8e2ff821b4a369eae2b04c70f
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-11-06 11:58:49 -0800

    Components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import components and data
+import { useState } from "react";
+import { projects } from "./data.jsx";
+
+// Import logos
+import reactLogo from "./assets/img/react.svg";
+import viteLogo from "./assets/img/vite.svg";
+
+// Import CSS
+import "./assets/css/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // We use the hook state to set the count
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/css/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Initializes the DOM, renders the App component and mounts it to the DOM.
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
